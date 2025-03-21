24c43c1843a69875d9d21e0d98781118d495df83
wookyungloh <lohwookyung@naver.com>
2025-03-21 20:47:30 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById('doctor-visit').addEventListener('click', function() {
+    document.getElementById('doctor-visit-info').classList.toggle('hidden');
+});
+
+document.getElementById('meal-time').addEventListener('click', function() {
+    document.getElementById('meal-time-info').classList.toggle('hidden');
+});
+
+document.getElementById('guardian-change').addEventListener('click', function() {
+    document.getElementById('guardian-change-info').classList.toggle('hidden');
+});
+
+document.getElementById('complaint').addEventListener('click', function() {
+    document.getElementById('complaint-info').classList.toggle('hidden');
+});
